5858810f604ec9579b2670dfa7496fb0b21bdf05
Yurii <lobacovurij@gmail.com>
2026-02-16 19:49:33 +0100

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+import { Header } from "./react/sections/header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/react/sections/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <div className="h-25">
+      <div className=" w-full h-full">
+        <svg />
+        <h1>Job Helper</h1>
+      </div>
+
+      <img src="#" alt="user account avatar image" />
+    </div>
+  );
+}
