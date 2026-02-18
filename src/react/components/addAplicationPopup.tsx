e7584aa9b08d8dee42bc302ea2ef5fe2d156d7de
Yurii <lobacovurij@gmail.com>
2026-02-18 10:47:03 +0100

    added popup

--- src/react/components/addAplicationPopup.tsx
+++ src/react/components/addAplicationPopup.tsx
@@ -5,12 +5,63 @@ export function AddAplicationPopup() {
   setPopupShowed;
   isPopupShowed;
 
-  /* if (isPopupShowed)  */ return (
-    <div className="fixed inset-0 top-1/2 left-1/2 -translate-1/2 bg-red-200">
-      <form action="">
-        <input type="text" />
-        <button type="submit">Submit</button>
-      </form>
+  const hidePopup = () => {
+setPopupShowed(false);
+} 
+const showPopup = () => {setPopupShowed(true);}
+
+  if (isPopupShowed)  return (
+    <div className="fixed inset-0 bg-black/50 z-2">
+      <div className=" fixed inset-0 top-1/2 left-1/2 -translate-1/2 h-auto bg-white rounded-md drop-shadow-x1 z-5">
+        <form className="w-full h-full flex flex-col justify-between" action="">
+          <div className="flex">
+            <label className="flex flex-col p-5 w-1/2" htmlFor="">
+              <span className="">Company name</span>
+              <input
+                type="text"
+                className="border rounded-md border-gray-500 h-10 pl-3"
+              />
+            </label>
+            <label className="flex flex-col p-5 w-1/2" htmlFor="">
+              <span className="">Position</span>
+              <input
+                type="text"
+                className="border rounded-md border-gray-500 h-10 pl-3"
+              />
+            </label>
+          </div>
+          <label className="flex flex-col px-5  w-full" htmlFor="">
+            <span className="">Companys Recruiting Email</span>
+            <input
+              type="email"
+              className="border rounded-md border-gray-500 h-8 pl-3"
+            />
+          </label>
+          <label className="flex flex-col px-5 py-2 w-full" htmlFor="">
+            <span className="">Short Job Description</span>
+            <textarea
+              name=""
+              id=""
+              className="border rounded-md border-gray-500 pl-3 h-22"
+            ></textarea>
+          </label>
+          <div className="flex m-3 self-end gap-3">
+            <button
+              type="button"
+              onClick={hidePopup}
+              className="bg-gray-500 text-white px-6 py-2 rounded-lg"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-700 text-white px-6 py-2 rounded-lg"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
