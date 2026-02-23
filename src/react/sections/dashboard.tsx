75dd2b82f4667f0719f565bd03b068fa20ed0c21
Yurii <lobacovurij@gmail.com>
2026-02-23 16:09:24 +0100

    added a adding cards realization

--- src/react/sections/dashboard.tsx
+++ src/react/sections/dashboard.tsx
@@ -1,21 +1,23 @@
 import { JobCard } from "../components/card";
-import { useState } from "react";
-import type { cardProps } from "../components/card"
+import type { cardProps } from "../../types/types";
 
-export function Dashboard() {
-const [cardsArray , changeCardArray] = useState<cardProps[]>([]);
-
-const addCard = () => {changeCardArray(prev => [...prev , {idTime :  new Date(),companyName : "fdfdsaaaafd" ,position : " dffdf" }])}
-
+export function Dashboard({ jobJSdataArr }: { jobJSdataArr: cardProps[] }) {
   return (
     <main className="grid grid-cols-3 gap-6 mt-5 mx-5">
       <section className=" flex flex-col gap-y-2">
         <div className="flex bg-blue-800 justify-center rounded-md px-6 py-3 ">
           <p className="text-[#FFFFFF] text-[16px] font-bold">Applied</p>
         </div>
+        <div className="flex gap-4"></div>
 
-        <div className="flex gap-4">
-        </div>
+        {/* TODO : Card put here */}
+        {jobJSdataArr.map((item) => (
+          <JobCard
+            idTime={item.idTime}
+            companyName={item.companyName}
+            position={item.position}
+          />
+        ))}
       </section>
 
       <section>
